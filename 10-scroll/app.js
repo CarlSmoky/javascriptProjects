a33e9cf8b7d9a746fb8c552a67341e9d67629910
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-17 16:44:05 -0400

    Make nav shown dynamically

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -7,7 +7,21 @@
 const date = document.querySelector('#date');
 date.innerHTML = new Date().getFullYear();
 // ********** close links ************
-
+const navToggle =document.querySelector('.nav-toggle');
+const linksContainer =document.querySelector('.links-container');
+const links =document.querySelector('.links');
+console.log(links);
+navToggle.addEventListener('click', () => {
+  // linksContainer.classList.toggle('show-links');
+  const containerHeight = linksContainer.getBoundingClientRect().height;
+  const linksHeight = links.getBoundingClientRect().height;
+  // If default height
+  if (containerHeight === 0) {
+    linksContainer.style.height = `${linksHeight}px`
+  } else {
+    linksContainer.style.height = 0;
+  }
+});
 // ********** fixed navbar ************
 
 // ********** smooth scroll ************
