bb4ea93feee6ab89c7d377e23f0411612a6c6add
tsumitakaoru <k.tsumita01@gmail.com>
2022-05-18 19:20:58 -0400

    scroll-link top-link

--- 10-scroll/app.js
+++ 10-scroll/app.js
@@ -34,6 +34,12 @@ window.addEventListener('scroll', () => {
   } else {
     navbar.classList.remove('fixed-nav');
   }
+
+  if (scrollHeight > 500) {
+    topLink.classList.add('show-link');
+  } else {
+    topLink.classList.remove('show-link');
+  }
 })
 // ********** smooth scroll ************
 // select links
